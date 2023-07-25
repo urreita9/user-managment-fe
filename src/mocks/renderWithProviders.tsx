fec0ee555ca75e4e3df5dafd61f2e7bb8f98bcff
francisco <francisco.urrea9@gmail.com>
2023-07-25 00:57:36 -0300

    bedoreEach => queryClient.clean()

--- src/mocks/renderWithProviders.tsx
+++ src/mocks/renderWithProviders.tsx
@@ -2,9 +2,7 @@ import { render } from "@testing-library/react"
 import React from "react"
 import { QueryClient, QueryClientProvider } from "react-query"
 
-const queryClient = new QueryClient()
-
-//queryClient.clear()
+export const queryClient = new QueryClient()
 
 export const renderWithProviders = (ui: React.ReactNode) =>
   render(<QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>)
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,8 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+import { queryClient } from "./mocks/renderWithProviders"
 
 import { server } from "./mocks/server"
+
+beforeEach(() => queryClient.clear())
 
 beforeAll(() => server.listen())
 
